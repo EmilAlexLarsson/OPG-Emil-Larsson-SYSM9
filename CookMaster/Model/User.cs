c0c9c13e9a547d2d733b1d9da12cee9448567d42
EmilAlexLarsson <emilalexlarsson@gmail.com>
2025-10-20 14:14:14 +0200

    User

---
+++ CookMaster/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookMaster.Model
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Country { get; set; }
+
+
+    }
+}
