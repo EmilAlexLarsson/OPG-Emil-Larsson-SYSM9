6b23823e6a947d64a2af67aaf78edff8e9cee21c
EmilAlexLarsson <emilalexlarsson@gmail.com>
2025-10-20 17:09:49 +0200

    Öppnar nytt fönster när man loggar in

--- CookMaster/ViewModel/MainWindowViewModel.cs
+++ CookMaster/ViewModel/MainWindowViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CookMaster.Manager;
 using CookMaster.MVVM;
+using CookMaster.View;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CookMaster.ViewModel
 {
@@ -16,5 +18,42 @@ namespace CookMaster.ViewModel
             UserManager = userManager;
         }
 
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+        public RelayCommand LogInCommand => new RelayCommand(execute => LogIn());
+        public RelayCommand RegisterCommand => new RelayCommand(execute => LogIn());
+
+        public void LogIn()
+        {
+            RecipeListWindow recipeListWindow = new RecipeListWindow();
+            if (UserManager.LogIn(Username, Password))
+            {
+                recipeListWindow.Show();
+                Application.Current.Windows[0].Close();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username or password");
+            }
+        }
+        
     }
 }
