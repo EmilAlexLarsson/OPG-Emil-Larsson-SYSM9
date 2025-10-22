c870aef06092a575cce1f23c533d2bad5a9024ad
EmilAlexLarsson <emilalexlarsson@gmail.com>
2025-10-22 23:19:36 +0200

    La till datacontext till recipelistviewmodel

--- CookMaster/View/RecipeListWindow.xaml.cs
+++ CookMaster/View/RecipeListWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookMaster.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace CookMaster.View
         public RecipeListWindow()
         {
             InitializeComponent();
+            RecipeListWindowViewModel recipeListViewModel = new RecipeListWindowViewModel();
+            DataContext = recipeListViewModel;
         }
     }
 }
